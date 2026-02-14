264f5644964bf3af4e7d6a7fbf0455d563b001b1
Renaud_AMAND <renaud.amand@gmail.com>
2026-02-15 00:03:42 +0100

    Ajout visuel

--- 00_Projets/Jeu_Alphabet/main.py
+++ 00_Projets/Jeu_Alphabet/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 
 pygame.init()
 
@@ -9,6 +10,7 @@ pygame.display.set_caption('Jeu Apprentissage Charlène')
 ma_police = pygame.font.SysFont("Arial", 350) # On le met en très gros !
 petite_police = pygame.font.SysFont("Arial", 100)
 screen_rect = screen.get_rect()
+hauteur = screen_rect.height
 
 alphabet = [
     ("A", "Avion"), ("B", "Ballon"), ("C", "Chat"), ("D", "Dauphin"),
@@ -28,17 +30,32 @@ while en_marche:
     screen.fill((255, 255, 255))
     
     lettre, objet = alphabet[index]
+
+    #On définit le chemin de l'image
+    chemin_image = f"images/{objet}.png"
+
+    if os.path.exists(chemin_image):
+        image_brute = pygame.image.load(chemin_image).convert_alpha()
+        image_dessin = pygame.transform.scale(image_brute, (300, 300))
+        rect_image = image_dessin.get_rect(centerx=screen_rect.centerx, centery = 180)
+        rect_image.centerx = screen_rect.centerx
+        rect_image.centery = hauteur * 0.25
+        screen.blit(image_dessin, rect_image)
+    else:
+        pass
+        
+
      # 1. On prépare la lettre (en gros)
     image_lettre = ma_police.render(lettre, True, (0, 0, 0))
     rect_lettre = image_lettre.get_rect()
     rect_lettre.centerx = screen_rect.centerx
-    rect_lettre.centery = screen_rect.centery - 100
+    rect_lettre.centery = screen_rect.centery + 50
     screen.blit(image_lettre, rect_lettre) 
     
     image_objet = petite_police.render(objet, True, (50, 50, 50))
     rect_objet = image_objet.get_rect()
     rect_objet.centerx = screen_rect.centerx
-    rect_objet.centery = screen_rect.centery + 150
+    rect_objet.bottom = screen_rect.bottom -40
     screen.blit(image_objet, rect_objet)
     # B. On rafraîchit l'écran
     pygame.display.flip()
