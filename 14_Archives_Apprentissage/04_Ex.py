e1c5c7ef4be009f6cf93c099ab3c692c5c996c47
Renaud_AMAND <renaud.amand@gmail.com>
2026-02-16 20:48:35 +0100

    Exercice

---
+++ 14_Archives_Apprentissage/04_Ex.py
@@ -0,0 +1,17 @@
+objets = ["Potion", "codeball", "Super Bonbon", "Rappel", "Super Potion"]
+
+for objet in objets:
+    print(f"Vérification {objet}")
+    if objet == "Super Bonbon":
+        print("Super Bonbon trouvé ! arrêt de la recherche")
+        break
+
+niveaux_puissance = [35, 62, 48, 55, 73, 42, 68]
+
+for niveau in niveaux_puissance:
+    if niveau < 50:
+        continue
+    print(f"Niveau de puissance {niveau}")
+
+
+print("---" * 3)
