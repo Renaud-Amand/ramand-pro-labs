8e368ce2bd90127dc9b62ea64a65071c63fe00ee
Renaud_AMAND <renaud.amand@gmail.com>
2026-02-20 17:28:06 +0100

    style: nettoyage de la structure des dossiers et archivage V1

--- 00_Projets/Snake_Project/snake.py
+++
@@ -1,9 +0,0 @@
-class Snake :
-    def __init__(self):
-        self.corps = [(100, 100), (90, 100), (80,100)]
-        slef.direction = "DROITE"
-
-    def avancer(selft):
-        tete_actuelle = self.corps[-1]
-        nouvelle_tete = (tete_actuelle[0] + 10, tete_actuelle[1])
-        
